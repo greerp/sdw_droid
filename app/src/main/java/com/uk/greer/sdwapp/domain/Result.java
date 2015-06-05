1a3d282baab7b093c59a261e76a9b8ab289310a7
Paul Greer <greepau@LDNLT00007>
2015-06-05 05:57:13 +0100

    Implemented Handicap and Scratch table standing data retrieval

---
+++ app/src/main/java/com/uk/greer/sdwapp/domain/Result.java
@@ -0,0 +1,7 @@
+package com.uk.greer.sdwapp.domain;
+
+/**
+ * Created by greepau on 02/06/2015.
+ */
+public class Result {
+}
---
+++ app/src/main/java/com/uk/greer/sdwapp/service/TimeTrialStandingMatrix.java
@@ -0,0 +1,7 @@
+package com.uk.greer.sdwapp.service;
+
+/**
+ * Created by greepau on 02/06/2015.
+ */
+public class TimeTrialStandingMatrix {
+}
