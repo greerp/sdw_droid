dba21239c053a54cebb61c89a3e15a2ad4f157cb
Paul Greer <paul.greer@redpixie.com>
2016-01-26 08:13:37 +0000

    Changes to dummy data generation

---
+++ sdw-functions.ps1
@@ -0,0 +1,53 @@
+﻿function get-Courses($basePath){
+    Import-Csv "${basePath}\courses.csv"
+}
+
+
+function get-Entries($basePath){
+    Import-Csv "${basePath}\entries.csv"
+}
+
+function get-Events($basePath){
+    Import-Csv "${basePath}\events.csv"
+}
+
+function get-Series($basePath){
+    Import-Csv "${basePath}\series.csv"
+}
+
+function get-Users($basePath){
+    Import-Csv "${basePath}\Users.csv"
+}
+
+
+function RandomIntBetween([int]$low, [int]$high) {
+    return Get-Random -Minimum $low -Maximum $high
+}
+
+function shuffle-list($list){
+    $count = $list.Count
+    $result = $list.Clone()
+
+    for($i=0; $i -lt $count; $i++){
+        $rnd = (Get-Random -Minimum 1 -Maximum 10)/10
+        $j = [int]((($count-1) - $i) * $rnd) + $i
+        $temp = $result[$i]
+        $result[$i]=$result[$j]
+        $result[$j]=$temp
+    }
+    return $result
+}
+
+
+function New-EventEntry{
+    $properties = @{
+        'id'="";
+        'date'="";
+        'eventid'="";
+        'courseId'="";
+    }
+    $event = New-Object –TypeName PSObject –Prop $properties
+    return $event
+}
+
+
---
+++ sdw-loaddata.ps1
@@ -0,0 +1,43 @@
+﻿. "$PSScriptRoot\sdw-functions.ps1"
+
+$root = "C:\Users\greepau\AndroidStudioProjects\sdw_droid"
+
+$handicaps = (0, 30, 60, 90, 120, 150, 180, 240, 270, 300)
+$courses =  get-Courses $root
+$users = get-Users $root
+$events = get-Events $root
+$series = get-Series $root
+
+#shuffle-list $users |format-Table
+#$users|format-table
+
+
+$entryid=0
+foreach ( $event in $events ) {
+    
+
+
+    $entry = New-EventEntry
+
+    $entry.id = ++$entryid
+    $entry.eventid = $event.id
+
+    
+
+
+
+    $r = RandomIntBetween 1 10
+    #Write-Host $r
+}
+
+
+
+
+
+
+
+
+
+
+
+
